2563b90f831b4db347741c1a13a23ba9553880c3
mizukishinmyou <vanmizuki39@gmail.com>
2023-04-25 16:50:02 +0900

    views.py に処理を追加

--- diary/views.py
+++ diary/views.py
@@ -1,6 +1,12 @@
+import logging
+
+from django.urls import reverse_lazy
+
 from django.views import generic
 
 from .forms import InquiryForm
+
+logger = logging.getLogger(__name__)
 
 
 class IndexView(generic.TemplateView):
@@ -10,3 +16,9 @@ class IndexView(generic.TemplateView):
 class InquiryView(generic.FormView):
     template_name = "inquiry.html"
     form_class = InquiryForm
+    success_url = reverse_lazy("diary:inquiry")
+
+    def form_invalid(self, form):
+        form.send_email()
+        logger.info("Inquiry sent by {}".format(form.cleaned_data["name"]))
+        return super().form_valid(form)
