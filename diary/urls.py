7e0616dfa4f5181cbbd1b31695638e830ab11584
mizukishinmyou <vanmizuki39@gmail.com>
2023-04-25 10:26:51 +0900

    inquiry が表示されないエラーを解決

--- diary/urls.py
+++ diary/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+
 from . import views
 
-app_name = "diary"
+
+app_name = 'diary'
 urlpatterns = [
-    path("", views.IndexView.as_view(), name="index"),
-    path("inquiry", views.IndexView.as_view(), name="inquiry"),
+    path('', views.IndexView.as_view(), name="index"),
+    path('inquiry/', views.InquiryView.as_view(), name="inquiry"),
 ]
